32f42c4992c06323aaee847460cd23f188a691ba
Vitaly <kva-nvk86@mail.ru>
2023-08-15 16:13:04 +0300

    Complete

---
+++ HomeWork_5/Program.cs
@@ -0,0 +1,169 @@
+﻿/*
+Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+*/
+/*
+int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int[] CreateArray(int size)
+{
+    return new int[size];
+}
+
+void FillArray(int[] array)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+        array[i] = rnd.Next(100, 1000);
+}
+
+string PrintArray(int[] array)
+{
+    string res = String.Empty;
+    for (int i = 0; i < array.Length; i++)
+        res += array[i] + " ";
+    return res;
+}
+
+
+int EvenNums(int[] array)
+{
+    int even = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] % 2 == 0)
+            even = even + 1;
+    return even;
+}
+
+
+
+int size = InputNum("Введите размер массива: ");
+int[] myArray = CreateArray(size);
+FillArray(myArray);
+string txt = PrintArray(myArray);
+Console.WriteLine(txt);
+int res = EvenNums(myArray);
+Console.WriteLine($"Количество чётных элементов в массиве равна {res}");
+*/
+
+
+/*
+Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+
+/*
+int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int[] CreateArray(int size)
+{
+    return new int[size];
+}
+
+void FillArray(int[] array)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+        array[i] = rnd.Next(1, 100);
+}
+
+string PrintArray(int[] array)
+{
+    string res = String.Empty;
+    for (int i = 0; i < array.Length; i++)
+        res += array[i] + " ";
+    return res;
+}
+
+
+int SumNums(int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i++)
+        if (i % 2 != 0)
+            sum += array[i];
+    return sum;
+}
+
+int size = InputNum("Введите размер массива: ");
+int[] myArray = CreateArray(size);
+FillArray(myArray);
+string txt = PrintArray(myArray);
+Console.WriteLine(txt);
+int res = SumNums(myArray);
+Console.WriteLine($"Сумма нечётных элементов в массиве равна {res}");
+*/
+
+
+/*
+Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементом массива.
+[3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+*/
+
+
+int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+double[] CreateArray(int size)
+{
+    return new double[size];
+}
+
+void FillArray(double[] array)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+        array[i] = rnd.Next(-10, 10);
+}
+
+
+string PrintArray(double[] array)
+{
+    string res = String.Empty;
+    for (int i = 0; i < array.Length; i++)
+        res += array[i] + " ";
+    return res;
+}
+
+
+double MaxNum(double[] array)
+{
+    double max = array[0];
+    for (int i = 1; i < array.Length; i++)
+        if (array[i] > max)
+            max = array[i];
+    return max;
+}
+
+double MinNum(double[] array)
+{
+    double min = array[0];
+    for (int i = 1; i < array.Length; i++)
+        if (array[i] < min)
+            min = array[i];
+    return min;
+}
+int size = InputNum("Введите размер массива: ");
+double[] myArray = CreateArray(size);
+FillArray(myArray);
+string txt = PrintArray(myArray);
+Console.WriteLine(txt);
+double maxNum = MaxNum(myArray);
+double minNum = MinNum(myArray);
+Console.WriteLine($"Значение максимального элемента массива  {maxNum}");
+Console.WriteLine($"Значение минимального элемента массива  {minNum}");
+double odds = maxNum - minNum;
+Console.WriteLine($"Разница между максимальным и минимальным элементами массива {odds}");
